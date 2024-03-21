cc75742cfe62c17eb54082b76fe9ec318148bd2b
B1ACK917 <397615103@qq.com>
2024-03-21 13:55:25 +0800

    Initial Commit

---
+++ Kaguya/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Kaguya
+//
+//  Created by 冯大纬 on 2024/3/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
